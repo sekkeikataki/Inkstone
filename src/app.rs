1153a7c20c2b06c770a5cc4823f0189dbe5ab73d
Radim Václavík <150831563+sekkeikataki@users.noreply.github.com>
2026-09-17 20:03:56 -0700

    Replace src/app.rs with include glue

--- src/app.rs
+++ src/app.rs
@@ -1,1 +1,5 @@
-TRUNCATED_FOR_SIZE_TEST
+include!("app/app_part01.inc.rs");
+include!("app/app_part02.inc.rs");
+include!("app/app_part03.inc.rs");
+include!("app/app_part04.inc.rs");
+include!("app/app_part05.inc.rs");
